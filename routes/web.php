76ece5055dc4622c6f0649aab13ba437a64ebd13
sujan kumal <setobhagera@gmail.com>
2018-03-06 11:45:03 +0545

    so

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,7 @@ Route::get('about', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
