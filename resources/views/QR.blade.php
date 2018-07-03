0ee12c1f50775463a6a46321bdd13489f55a4702
sujan kumal <setobhagera@gmail.com>
2018-07-03 21:40:54 +0545

    Qr generation form permission changed

--- resources/views/QR.blade.php
+++ resources/views/QR.blade.php
@@ -12,21 +12,27 @@
                     </a>
                     </div>
           @endif
-          <script src="{{ asset("js/autho.js")}}" type="text/javascript"></script>
-          <div class="form-group{{ $errors->has('project_id') ? ' has-error' : '' }}">
-                    <label for="project_id" class="col-md-4 control-label white-text">Project Name</label>
+            <script src="{{ asset("js/autho.js")}}" type="text/javascript"></script>
+           
+            @if(session()->has('sidebarProjectSelectedResponsePID'))
+                    
+                <div class="form-group{{ $errors->has('project_id') ? ' has-error' : '' }}">
+                    <label for="project_id" class="col-md-4 control-label">Project Name</label>
                     <div class="col-md-6">
                         <select id="project_id" name ="project_id" class="form-control" onchange="authoQRPS();">
-                            <option value=""></option>
-                            @foreach(App\project_detail::all() as $project)
-                                <option value="{{$project->id}}">{{$project->name}}</option>
-                            @endforeach
+                           <script type="text/javascript"> 
+                            $(document).ready(function() {
+                                    authoQRPS();
+                                });
+                           </script>
+                            <option value=" {{ session()->get('sidebarProjectSelectedResponsePID') }} ">{{session()->get('sidebarProjectSelectedResponseCP')}}</option>
                         </select>
                         @if ($errors->has('project_id'))
                             <span class="help-block"><strong>{{ $errors->first('project_id') }}</strong></span>
                         @endif
                     </div>
-                </div>
+                    </div>
+                @endif
                 <div class="form-group">
                     <div class="col-md-6 col-md-offset-4">
                         <button type="submit" class="btn btn-md btn-success" form="QRForm">QR Create</button>
