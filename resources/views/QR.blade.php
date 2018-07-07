aa69189e2af07f002415a297002ade98aa251a7e
sujan kumal <setobhagera@gmail.com>
2018-07-07 17:38:54 +0545

    qr gen scan ui changed

--- resources/views/QR.blade.php
+++ resources/views/QR.blade.php
@@ -2,20 +2,21 @@
 @section('page_heading','Dashboard')
 
 @section('section')
-<div id="qrform" >
+<div id="qrform">
 	<form id = "QRForm" class="form-horizontal" method="POST" action="/QRCreate">
-		{{ csrf_field() }}
+		<div class="row container-fluid jumbotron panel opag">   
+        {{ csrf_field() }}
 		 @if(session()->has('qrfilename'))
-                    <div class="alert alert-success">
+                <div class="alert alert-success">
                    	<a href="qrs/{{session()->get('qrfilename')}}">
                       <img src="qrs/{{session()->get('qrfilename')}}" id="qrimage">
                     </a>
-                    </div>
+                </div>
           @endif
             <script src="{{ asset("js/autho.js")}}" type="text/javascript"></script>
            
             @if(session()->has('sidebarProjectSelectedResponsePID'))
-                    
+            
                 <div class="form-group{{ $errors->has('project_id') ? ' has-error' : '' }}">
                     <label for="project_id" class="col-md-4 control-label">Project Name</label>
                     <div class="col-md-6">
@@ -31,14 +32,14 @@
                             <span class="help-block"><strong>{{ $errors->first('project_id') }}</strong></span>
                         @endif
                     </div>
-                    </div>
+                </div>
                 @endif
                 <div class="form-group">
                     <div class="col-md-6 col-md-offset-4">
                         <button type="submit" class="btn btn-md btn-success" form="QRForm">QR Create</button>
                     </div>
                 </div>
-		
+		</div> 
 	</form>
 
 </div>
--- resources/views/QRScan.blade.php
+++ resources/views/QRScan.blade.php
@@ -5,52 +5,42 @@
 <script type="text/javascript" src="{{ asset('js/qrcode.js') }}"></script>
 <script type="text/javascript" src="{{ asset('js/qr.js') }}"></script>
 <meta name="_token" content="{{ csrf_token() }}">
-<div id="main">
-        <div id="mainbody" style="display: inline;">
-            <table class="tsel" width="100%" border="0">
-            <tbody>
-            <tr>
-                <td width="50%" valign="top" align="center">
-                    <table class="tsel" border="0">
-                        <tbody>
-                        <tr>
-                        <td>
-                        <BUTTON class="selector" id="webcamimg" onclick="setwebcam()" 
-                        	style="opacity: 1;" 
-                            align="left" >Rescan</BUTTON>
-                        </td>
-                        
-                        </tr>
-                         <tr>
-                             <td colspan="2" align="center">
-                                 <div id="outdiv" style="width: 320px;
-                                 height: 240px;">
-                                     <video id="v"  autoplay="autoplay" ></video>
-                                </div>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-             </td>
-             </tr>
-             <tr>
-                 <td colspan="3" align="center">
+<div id="main" >
+    <div id="mainbody" >
+            <table class="tsel row container jumbotron panel mx-auto text-center" width="100%" border="0" >
+                
+                <tr>
+                    <td colspan="2" align="center">
+                        <hr>
+                        <div id="outdiv" style="width: 320px;height: 240px;">
+                          <video id="v"  autoplay="autoplay" ></video>
+                         </div>
+                         <hr>
+                     </td>
+                </tr>
+                <tr>
+                    <td colspan="3" align="center">
                      <div id="result">- scanning -</div>
                     </td>
                 </tr>
-            </tbody>
-        </table> 
-                    <script src="{{ asset("js/autho.js")}}" type="text/javascript"></script>
+                <tr>
+                    <td class="row container panel opag mx-auto text-center">
+                     <BUTTON  id="webcamimg" onclick="setwebcam()" 
+                                    >Rescan</BUTTON>
+                    </td>
+                </tr>
+            </table> 
+                    <!-- <script src="{{ asset("js/autho.js")}}" type="text/javascript"></script> -->
                 @if(session()->has('sidebarProjectSelectedResponsePID'))
                     
-                    <div class="form-group{{ $errors->has('project_id') ? ' has-error' : '' }}">
+                    <div class="form-group{{ $errors->has('project_id') ? ' has-error' : '' }} row container jumbotron panel">
                     <label for="project_id" class="col-md-4 control-label">Project Name</label>
                     <div class="col-md-6">
                         <select id="project_id" name ="project_id" class="form-control" onchange="load();authoQRScanPS();">
                            <script type="text/javascript"> 
                             $(document).ready(function() {
                                     load();
-                                    authoQRScanPS();
+                                    // authoQRScanPS();
                                 });
                            </script>
                             <option value=" {{ session()->get('sidebarProjectSelectedResponsePID') }} ">{{session()->get('sidebarProjectSelectedResponseCP')}}</option>
@@ -65,8 +55,8 @@
                 @endif
                   
     </div>
-    <canvas id="qr-canvas" width="800" height="600" style="width: 320px;  height: 240px; display:none;"></canvas>
+    <canvas id="qr-canvas" width="800" height="600" style="width: 320px;  height: 240px; display:none;" ></canvas>
     <script type="text/javascript">load();</script>
-
+</div>
 
 @stop
