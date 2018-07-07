22ce43424b7970578a3de8fdb4d63ccdb9131ab1
sujan kumal <setobhagera@gmail.com>
2018-07-07 14:26:12 +0545

    minute form ui changed

--- public/js/my.js
+++ public/js/my.js
@@ -111,84 +111,53 @@ function projectSelected() {
             console.log(typeof(resp.forMinuteResponseTask));
             $.each(resp.forMinuteResponseTask, function(index, value) {
                 if (value.task_complete==null) {
-                    var acheckbox = document.createElement('input'); 
-                    var alabel = document.createElement('label');
-                    acheckbox.type= 'checkbox';
-                    acheckbox.name = "leadername[]";
-                    acheckbox.value = value.id;
-                    acheckbox.id = "leadercheckbox"+value.id;
-                    alabel.setAttribute("for","leadercheckbox"+value.id);
-                    alabel.innerHTML = value.task;
-                    alabel.appendChild(acheckbox);
-                    var span = document.createElement('span');
-                    span.setAttribute("class","minuteCheckbox")
-                    span.appendChild(alabel);
-                    document.getElementById('leader_acheivement').appendChild(span);  
-                    var bcheckbox = document.createElement('input'); 
-                    var blabel = document.createElement('label');
-                    bcheckbox.type= 'checkbox';
-                    bcheckbox.name = "m1name[]";
-                    bcheckbox.value = value.id;
-                    bcheckbox.id = "m1checkbox"+value.id;
-                    blabel.setAttribute("for","m1checkbox"+value.id);
-                    blabel.innerHTML = value.task;
-                    blabel.appendChild(bcheckbox);
-                    var span1 = document.createElement('span');
-                    span1.setAttribute("class","minuteCheckbox")
-                    span1.appendChild(blabel);
-                    document.getElementById('member_i_acheivement').appendChild(span1); 
-                    var ccheckbox = document.createElement('input'); 
-                    var clabel = document.createElement('label');
-                    ccheckbox.type= 'checkbox';
-                    ccheckbox.name = "m2name[]";
-                    ccheckbox.value = value.id;
-                    ccheckbox.id = "m2checkbox"+value.id;
-                    clabel.setAttribute("for","m2checkbox"+value.id);
-                    clabel.innerHTML = value.task;
-                    clabel.appendChild(ccheckbox);
-                    var span2 = document.createElement('span');
-                    span2.setAttribute("class","minuteCheckbox")
-                    span2.appendChild(clabel);
-                    document.getElementById('member_ii_acheivement').appendChild(span2); 
-                    var dcheckbox = document.createElement('input'); 
-                    var dlabel = document.createElement('label');
-                    dcheckbox.type= 'checkbox';
-                    dcheckbox.name = "lrname[]";
-                    dcheckbox.value = value.id;
-                    dcheckbox.id = "lrcheckbox"+value.id;
-                    dlabel.setAttribute("for","lrcheckbox"+value.id);
-                    dlabel.innerHTML = value.task;
-                    dlabel.appendChild(dcheckbox);
-                    var span3 = document.createElement('span');
-                    span3.setAttribute("class","minuteCheckbox")
-                    span3.appendChild(dlabel);
-                    document.getElementById('leader_responsibility').appendChild(span3); 
-                    var echeckbox = document.createElement('input'); 
-                    var elabel = document.createElement('label');
-                    echeckbox.type= 'checkbox';
-                    echeckbox.name = "m1rname[]";
-                    echeckbox.value = value.id;
-                    echeckbox.id = "m1rcheckbox"+value.id;
-                    elabel.setAttribute("for","m1rcheckbox"+value.id);
-                    elabel.innerHTML = value.task;
-                    elabel.appendChild(echeckbox);
-                    var span4 = document.createElement('span');
-                    span4.setAttribute("class","minuteCheckbox")
-                    span4.appendChild(elabel);
-                    document.getElementById('member_i_responsibility').appendChild(span4); 
-                    var fcheckbox = document.createElement('input'); 
-                    var flabel = document.createElement('label');
-                    fcheckbox.type= 'checkbox';
-                    fcheckbox.name = "m2rname[]";
-                    fcheckbox.value = value.id;
-                    fcheckbox.id = "m2rcheckbox"+value.id;
-                    flabel.setAttribute("for","m2rcheckbox"+value.id);
-                    flabel.innerHTML = value.task;
-                    flabel.appendChild(fcheckbox);
-                    var span5 = document.createElement('span');
-                    span5.setAttribute("class","minuteCheckbox")
-                    span5.appendChild(flabel);
-                    document.getElementById('member_ii_responsibility').appendChild(span5); 
+                    $('#leader_acheivement').append(
+                        "<div class=\"form-check \">"+
+                        "<span  for=\"leadercheckbox"+value.id+"\">"+
+                        "</span>"+
+                        "<input type=\"checkbox\" class=\"form-check-input \" name=\"leadername[]\" value=\""+value.id+"\" id=\"leadercheckbox"+value.id+"\">"+
+                        value.task+
+                        "</div>");
+                    $('#member_i_acheivement').append(
+                        "<div class=\"form-check \">"+
+                        "<span  for=\"m1checkbox"+value.id+"\">"+
+                        "</span>"+
+                        "<input type=\"checkbox\" class=\"form-check-input \" name=\"m1name[]\" value=\""+value.id+"\" id=\"m1checkbox"+value.id+"\">"+
+                        value.task+
+                        "</div>");
+                    
+                    $('#member_ii_acheivement').append(
+                        "<div class=\"form-check \">"+
+                        "<span  for=\"m2checkbox"+value.id+"\">"+
+                        "</span>"+
+                        "<input type=\"checkbox\" class=\"form-check-input \" name=\"m2name[]\" value=\""+value.id+"\" id=\"m2checkbox"+value.id+"\">"+
+                        value.task+
+                        "</div>");
+                    
+                    $('#leader_responsibility').append(
+                        "<div class=\"form-check \">"+
+                        "<span  for=\"lrcheckbox"+value.id+"\">"+
+                        "</span>"+
+                        "<input type=\"checkbox\" class=\"form-check-input \" name=\"lrname[]\" value=\""+value.id+"\" id=\"lrcheckbox"+value.id+"\">"+
+                        value.task+
+                        "</div>");
+                    
+                    $('#member_i_responsibility').append(
+                        "<div class=\"form-check \">"+
+                        "<span  for=\"m1rcheckbox"+value.id+"\">"+
+                        "</span>"+
+                        "<input type=\"checkbox\" class=\"form-check-input \" name=\"m1rname[]\" value=\""+value.id+"\" id=\"m1rcheckbox"+value.id+"\">"+
+                        value.task+
+                        "</div>");
+                    
+                   $('#member_ii_responsibility').append(
+                        "<div class=\"form-check \">"+
+                        "<span  for=\"m2rcheckbox"+value.id+"\">"+
+                        "</span>"+
+                        "<input type=\"checkbox\" class=\"form-check-input \" name=\"m2rname[]\" value=\""+value.id+"\" id=\"m2rcheckbox"+value.id+"\">"+
+                        value.task+
+                        "</div>");
+                    
                     }
              }); 
         },
--- resources/views/minuteForm.blade.php
+++ resources/views/minuteForm.blade.php
@@ -2,9 +2,7 @@
 @section('page_heading','Minute Form')
 
 @section('section')
-<div class="container">
-    <div class="row">
-        <div panel-body bg-info>
+<div class="row container-fluid jumbotron panel">
             <form id="minuteForm" class="form-horizontal" method="POST" action="/minuteCreate" enctype="multipart/form-data">
               {{ csrf_field() }}
                 @if(session()->has('messageMinuteCreate'))
@@ -15,8 +13,7 @@
                 <script src="{{ asset("js/autho.js")}}" type="text/javascript"></script>
                 
                 @if(session()->has('sidebarProjectSelectedResponsePID'))
-                    
-                    <div class="form-group{{ $errors->has('project_id') ? ' has-error' : '' }}">
+                <div class="form-group{{ $errors->has('project_id') ? ' has-error' : '' }}">
                     <label for="project_id" class="col-md-4 control-label">Project Name</label>
                     <div class="col-md-6">
                         <select id="project_id" name ="project_id" class="form-control" onchange="projectSelected();authoMFPS();">
@@ -27,22 +24,19 @@
                                 });
                            </script>
                             <option value=" {{ session()->get('sidebarProjectSelectedResponsePID') }} ">{{session()->get('sidebarProjectSelectedResponseCP')}}</option>
-                            <!-- @foreach(App\project_detail::all() as $project)
-                                <option value="{{$project->id}}">{{$project->name}}</option>
-                            @endforeach -->
+                           
                         </select>
                         @if ($errors->has('project_id'))
                             <span class="help-block"><strong>{{ $errors->first('project_id') }}</strong></span>
                         @endif
                     </div>
-                    </div>
+                </div>
                 @endif
                 
                 <div class="form-group{{ $errors->has('agenda') ? ' has-error' : '' }}">
                     <label for="agenda" class="col-md-4 control-label">Agenda</label>
                     <div class="col-md-6">
-                        <input id="agenda" type="text" class="form-control" name="agenda" value="{{ old('agenda') }}" required >
-
+                        <textarea  id="agenda" type="textarea" class="form-control" name="agenda" value="{{ old('agenda') }}" required ></textarea>
                     @if ($errors->has('agenda'))
                             <span class="help-block"><strong>{{ $errors->first('agenda') }}</strong></span>
                         @endif
@@ -51,8 +45,7 @@
                 <div class="form-group{{ $errors->has('discussion') ? ' has-error' : '' }}">
                     <label for="discussion" class="col-md-4 control-label">Discussion</label>
                     <div class="col-md-6">
-                        <input id="discussion" type="text" class="form-control" name="discussion" value="{{ old('discussion') }}" required >
-
+                        <textarea id="discussion" type="text" class="form-control" name="discussion" value="{{ old('discussion') }}" required></textarea>
                     @if ($errors->has('discussion'))
                             <span class="help-block"><strong>{{ $errors->first('discussion') }}</strong></span>
                         @endif
@@ -61,11 +54,12 @@
 
                 <div class="form-group{{ $errors->has('leader_acheivement') ? ' has-error' : '' }}">
                     <label for="leader_acheivement" class="col-md-4 control-label">Acheivement of Leader</label>
-                    <div class="col-md-6">
+                    <div class="col-md-6" id="mfs">
                         
                         <div id="leader_acheivement" name ="leader_acheivement" class="minuteFormDispTask" >
-                            
+                                
                         </div>
+                        
                         @if ($errors->has('leadername'))
                             <span class="help-block"><strong>{{ $errors->first('leadername') }}</strong></span>
                         @endif
@@ -73,7 +67,7 @@
                 </div>
                 <div class="form-group{{ $errors->has('member_i_acheivement') ? ' has-error' : '' }}">
                     <label for="member_i_acheivement" class="col-md-4 control-label">Acheivement of Member 1st</label>
-                    <div class="col-md-6">
+                    <div class="col-md-6" id="mfs">
                        
                         <div id="member_i_acheivement" name ="member_i_acheivement" class="minuteFormDispTask" >
                             
@@ -85,7 +79,7 @@
                 </div>
                 <div class="form-group{{ $errors->has('member_ii_acheivement') ? ' has-error' : '' }}">
                     <label for="member_ii_acheivement" class="col-md-4 control-label">Acheivement of Member 2nd</label>
-                    <div class="col-md-6">
+                    <div class="col-md-6" id="mfs">
                         
                         <div id="member_ii_acheivement" name ="member_ii_acheivement" class="minuteFormDispTask" >
                             
@@ -97,7 +91,7 @@
                 </div>
                 <div class="form-group{{ $errors->has('leader_responsibility') ? ' has-error' : '' }}">
                     <label for="leader_responsibility" class="col-md-4 control-label">Responsibility of Leader</label>
-                    <div class="col-md-6">
+                    <div class="col-md-6" id="mfs">
                         
                         <div id="leader_responsibility" name ="leader_responsibility" class="minuteFormDispTask" >
                             
@@ -109,7 +103,7 @@
                 </div>
                 <div class="form-group{{ $errors->has('member_i_responsibility') ? ' has-error' : '' }}">
                     <label for="member_i_responsibility" class="col-md-4 control-label">Responsibility of Member 1st</label>
-                    <div class="col-md-6">
+                    <div class="col-md-6" id="mfs">
                         
                         <div id="member_i_responsibility" name ="member_i_responsibility" class="minuteFormDispTask" >
                             
@@ -121,7 +115,7 @@
                 </div>
                 <div class="form-group{{ $errors->has('member_ii_responsibility') ? ' has-error' : '' }}">
                     <label for="member_ii_responsibility" class="col-md-4 control-label">Responsibility of Member 2nd</label>
-                    <div class="col-md-6">
+                    <div class="col-md-6" id="mfs">
                         
                         <div id="member_ii_responsibility" name ="member_ii_responsibility" class="minuteFormDispTask" >
                             
@@ -136,7 +130,7 @@
                     <div class="col-md-6">
                         
                         <input data-preview="#preview" name="imageInput" type="file" id="imageInput">
-                        <img class="col-sm-6" id="preview"  src="" ></img>
+                        <img class="col-sm-6" id="preview"  src="" >
                         @if ($errors->has('imageInput'))
                             <span class="help-block"><strong>{{ $errors->first('imageInput') }}</strong></span>
                         @endif
@@ -149,6 +143,4 @@
                 </div>
             </form>
         </div>
-    </div>
-</div>
 @stop
