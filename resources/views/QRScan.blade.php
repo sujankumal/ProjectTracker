7e9ad3700dda292e4b3a3ea823d6d9697b3f5b80
sujan kumal <setobhagera@gmail.com>
2018-08-07 16:28:53 +0545

    mobile code

--- resources/views/QRScan.blade.php
+++ resources/views/QRScan.blade.php
@@ -6,9 +6,9 @@
 <script type="text/javascript" src="{{ asset('js/qr.js') }}"></script>
 <meta name="_token" content="{{ csrf_token() }}">
     
-<div class="col-md-12" id="main" >
-    <div class="box box-primary" id="mainbody" >
-        <div class="box-body">
+<div class="col-md-12" >
+    <div class="box box-primary" id="main">
+        <div class="box-body" id="mainbody">
          
            <table class="tsel container-fluid jumbotron panel mx-auto text-center" width="100%" border="0" >
                 
@@ -23,13 +23,20 @@
                 </tr>
                 <tr>
                     <td colspan="3" align="center">
-                     <div id="result">- scanning -</div>
+                     Result: <span id="result">scan</span>
                     </td>
                 </tr>
                 <tr>
                     <td class="row container panel opag mx-auto text-center">
                      <BUTTON  id="webcamimg" onclick="setwebcam()" 
                                     >Rescan</BUTTON>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <span class="col-xs-12 container-fluid jumbotron panel text-center"><i class="fa fa-qrcode" aria-hidden="true" id="qrcode-icon" style="display: none"><input type="file" accept="image/*" id="file-input" class="form-control-file col-xs-10 pull-right"  capture="environment" style="display: none"></i>
+                        </span>
+                        
                     </td>
                 </tr>
             </table> 
@@ -41,7 +48,11 @@
                             <select id="project_id" name ="project_id" class="form-control" onchange="load();authoQRScanPS();">
                                <script type="text/javascript"> 
                                 $(document).ready(function() {
-                                        load();
+                                        if (isMobile) {
+                                            loadMobile();
+                                        }else{
+                                            load(); 
+                                        }
                                         authoQRScanPS();
                                     });
                                </script>
@@ -55,11 +66,12 @@
                         </div>
                     </div>
                 @endif
+                  
              
     </div>
     <canvas id="qr-canvas" width="800" height="600" style="width: 320px;  height: 240px; display:none;" ></canvas>
-    </div>
+    
     <script type="text/javascript">load();</script>
 </div>
-
+</div>
 @stop
